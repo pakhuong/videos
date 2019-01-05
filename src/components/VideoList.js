5c2370d288ee427af2a989905e64b212b8b70ce6
pakhuong <anhkhuong1602@gmail.com>
2019-01-05 16:43:41 +0700

    Render video thumbnails

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -3,7 +3,7 @@ import React from 'react';
 import VideoItem from './VideoItem';
 
 const VideoList = ({ videos }) => {
-    const renderedList = videos.map(video => <VideoItem />);
+    const renderedList = videos.map(video => <VideoItem key={video.id.videoId} video={video} />);
 
     return (
         <div>{renderedList}</div>
