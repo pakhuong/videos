1a14a122355c64c366815f89a083995562c8d2f5
pakhuong <anhkhuong1602@gmail.com>
2019-01-06 23:32:47 +0700

    Add some styling to the video list

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -6,7 +6,7 @@ const VideoList = ({ videos }) => {
     const renderedList = videos.map(video => <VideoItem key={video.id.videoId} video={video} />);
 
     return (
-        <div>{renderedList}</div>
+        <div className="ui relaxed divided list">{renderedList}</div>
     );
 };
 
