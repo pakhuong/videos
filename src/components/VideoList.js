9546184cea115efccfbef66c987517c92c8ef9ce
pakhuong <anhkhuong1602@gmail.com>
2019-01-13 23:59:59 +0700

    Apply Semantic UI grid to render selected video and video list side by side

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -3,11 +3,15 @@ import React from 'react';
 import VideoItem from './VideoItem';
 
 const VideoList = ({ videos, onVideoSelect }) => {
-    const renderedList = videos.map(video => <VideoItem key={video.id.videoId} video={video} onVideoSelect={onVideoSelect} />);
+    const renderedList = videos.map(video => (
+        <VideoItem
+            key={video.id.videoId}
+            video={video}
+            onVideoSelect={onVideoSelect}
+        />
+    ));
 
-    return (
-        <div className="ui relaxed divided list">{renderedList}</div>
-    );
+    return <div className="ui relaxed divided list">{renderedList}</div>;
 };
 
 export default VideoList;
